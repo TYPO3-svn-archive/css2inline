1f8952a4b78bf880ab8b66445b4ce9e1cf3a807d
oschopfer <oschopfer@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-01-26 15:37:24 +0000

    First upload.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/css2inline/trunk@16093 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "css2inline"
+#
+# Auto generated 26-01-2009 08:51
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'CSS to inline converter for direct mail',
+	'description' => 'Moves the styles from CSS sheets into inline CSS, in order to comply with uncooperative email clients. Can be used as a post-processing stdWrap in TypoScript.',
+	'category' => 'plugin',
+	'author' => 'Olivier Schopfer',
+	'author_email' => 'ops@wcc-coe.org',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"d6f0";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"3601";s:19:"doc/wizard_form.dat";s:4:"3ca4";s:20:"doc/wizard_form.html";s:4:"9b71";s:31:"pi1/class.tx_css2inline_pi1.php";s:4:"4d34";}',
+);
+
+?>
